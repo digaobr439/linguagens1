465cb2e90a66ff8f2eb69cbe5a37bac430f8452c
Rodrigo <digaoelegal@gmail.com>
2018-03-12 12:56:38 -0300

    exercicio 4

---
+++ pratica1/exercicio4/Aula03/src/aula03/Aula03.java
@@ -0,0 +1,19 @@
+package aula03;
+
+/**
+ *
+ * @author 16.03378-7
+ */
+public class Aula03 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("Hello, Netbeans");
+    
+             
+    }
+    
+}
